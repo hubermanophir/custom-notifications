79a99c58a1d70c5b2bb541e1c1922ac65cfdd2dd
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-08-23 14:45:08 +0300

    Added socket init in server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,27 @@
+require("dotenv").config();
 const app = require("./app");
+const server = require("http").createServer(app);
 const PORT = 8080;
+const mongoose = require("mongoose");
 
-app.listen(PORT, () => {
+//Defining global io for socket
+global.io = require("socket.io")(server, {
+  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+});
+
+io.on("connection", (socket) => {
+  console.log("socket connected");
+});
+
+server.listen(PORT, () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log("Connected Successfully To MongoDB "))
+    .catch(() => console.error("Not Connected"));
   console.log(`Listening on port: ${PORT}`);
 });
