9f8e41e024c1e2d4de3f8273c8f5da4be64d8964
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-08-25 15:12:11 +0300

    moved server to be the main repo

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(`${__dirname}/../client/build`));
+app.use(express.static(`${__dirname}/client/build`));
 
 app.use("/api", api);
 
