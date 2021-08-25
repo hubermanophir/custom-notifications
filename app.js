0c406dd133b49fd548f24dbc1fe04850063d7cc1
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-08-25 16:56:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const api = require("./routes");
 const app = express();
 const cors = require("cors");
 
+app.use(express.static(`${__dirname}/client/build`));
 app.use(cors());
 app.use(express.json());
-
-app.use(express.static(`${__dirname}/client/build`));
 
 app.use("/api", api);
 
